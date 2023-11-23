a5ff852512b92274c9a3d14cf9ac8730aa058e26
PaniOdProgramowania <pani.od.programowania@gmail.com>
2023-11-23 08:00:00 +0200

    Update homework
    Update homework
    
    Add comments for homework purposes

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Zadanie domowe:
-1. Uzupełnij w poprawnej kolejności, co się wypisze na konsolę:
-https://wordwall.net/play/14220/931/600
+1. W jakiej kolejności wywoływane są bloki kodu w Javie? Ogólny przypadek
+https://wordwall.net/play/14223/604/900
 
-2. Jaka jest kolejność uruchamiania bloków w Javie? Uporządkuj
-https://wordwall.net/pl/resource/14221616
+2. W jakiej kolejności wywoływane są bloki kodu w Javie? Uporządkuj
+https://wordwall.net/play/14220/931/476
--- src/pl/paniodprogramowania/inheritance/Main.java
+++ src/pl/paniodprogramowania/inheritance/Main.java
@@ -3,13 +3,22 @@ package pl.paniodprogramowania.inheritance;
 public class Main {
 
   public static void main(String[] args) {
+    // Operacja A
     Animal animal = new Animal("rather not say");
+
+    // Operacja B
     Animal animalMonkey = new Monkey("monkey gender");
+
+    // Operacja C
     Animal animalHuman = new Human("human gender");
 
+    // Operacja D
     Monkey monkey = new Monkey("W");
+
+    // Operacja E
     Monkey monkeyHuman = new Human("M");
 
+    // Operacja F
     Human human = new Human("G");
   }
 }
